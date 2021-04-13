ad8da62964193e98fc5c018ea86d3b4cf4b42715
jackingham <jackingham1998@gmail.com>
2021-04-13 10:56:31 +0100

    Added boilerplate classes

--- src/main/java/com/sparta/eng82/App.java
+++ src/main/java/com/sparta/eng82/App.java
@@ -1,11 +1,18 @@
 package com.sparta.eng82;
 
+import com.sparta.eng82.objects.TrainingCentre;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
+public class App {
+
+    private List<TrainingCentre> listOfCentres = new ArrayList<>();
+
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
---
+++ src/main/java/com/sparta/eng82/objects/Trainee.java
@@ -0,0 +1,21 @@
+package com.sparta.eng82.objects;
+
+public class Trainee {
+    private final int id;
+    private final int joinMonth;
+    private static int idIncrementor = 0;
+
+
+    public Trainee(int joinMonth) {
+        this.joinMonth = joinMonth;
+        id = idIncrementor++;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getJoinMonth() {
+        return joinMonth;
+    }
+}
