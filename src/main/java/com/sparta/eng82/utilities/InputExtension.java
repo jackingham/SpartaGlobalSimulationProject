f7374338d1fefde787338ddbea5390e1ca6c5b88
alexsikorski <alexsikorski22@gmail.com>
2021-04-14 16:38:54 +0100

    Added InputExtension class. Pair programmed by Alex and Abdul, Jakub and Teni.

---
+++ src/main/java/com/sparta/eng82/utilities/InputExtension.java
@@ -0,0 +1,13 @@
+package com.sparta.eng82.utilities;
+
+import java.util.Scanner;
+
+public class InputExtension {
+
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static boolean outputEveryMonth(){
+        Printer.printMessage("Enter y/n if you would like to print output every month");
+        return scanner.nextLine().equals("y");
+    }
+}
