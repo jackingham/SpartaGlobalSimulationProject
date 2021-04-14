e6e2a557fc412b96aa27735b4a5c374c92ee5c09
Golam Choudhury <19grc97@gmail.com>
2021-04-14 16:50:44 +0100

    Added a CentreManager class to handle the closing of centres - Golam and Patrick

---
+++ src/main/java/com/sparta/eng82/controller/CentreManager.java
@@ -0,0 +1,39 @@
+package com.sparta.eng82.controller;
+
+import com.sparta.eng82.model.Bootcamp;
+import com.sparta.eng82.model.Trainee;
+import com.sparta.eng82.model.TrainingCentre;
+
+import java.util.List;
+
+public class CentreManager {
+
+    public static void closeCentre(List<TrainingCentre> centres) {
+        /*TrainingCentre centreWithSpace1;
+        TrainingCentre centreWithSpace2;*/
+        for (TrainingCentre centre: centres) {
+            int numberOfTrainees = centre.getTraineeArraySize();
+            if (numberOfTrainees <= 10) {
+                if (centre.centreType() == Bootcamp) {
+                    if (centre.monthsActive == 3) {
+                        getCentreWithSpace(centre.getTraineeArray());
+                        centre.isOpen = false;
+                    } else {
+                        centre.monthsActive++;
+                    }
+                } else {
+                    getCentreWithSpace(centre.getTraineeArray());
+                    centre.isOpen() = false;
+                }
+            } else {
+                centreWithSpace1.getTraineeArray().addAll(centre.getTraineeArray());
+            }
+        }
+    }
+
+    public static void getCentreWithSpace(Trainee[]) {
+
+    }
+
+
+}
