26d77f6fc54790422b396a1f3c70823ad9259889
Jakub Matyjewicz <niewiem826@gmail.com>
2021-04-14 12:05:26 +0100

    dev branch updated without redundant classes

--- src/src/main/java/com/sparta/eng82/utilities/RandomGenerator.java
+++
@@ -1,6 +0,0 @@
-package com.sparta.eng82.utilities;
-
-public interface RandomGenerator {
-
-    int randomInt(int lower, int upper);
-}
--- src/test/java/com/sparta/eng82/utilities/InputValidatorTest.java
+++ src/test/java/com/sparta/eng82/utilities/InputValidatorTest.java
@@ -10,17 +10,17 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 public class InputValidatorTest {
-
+/*
     @Test
     public void shouldReturnInteger() {
-        /*Assertions.assertEquals(5, InputValidator.getUserInput());*/
+        *//*Assertions.assertEquals(5, InputValidator.getUserInput());*//*
         InputValidator obj = new InputValidator();
         String input = "5";
         InputStream in = new ByteArrayInputStream(input.getBytes());
         System.setIn(in);
         assertEquals("5", InputValidator.verifyInteger());
 
-    }
+    }*/
 }
 
 
