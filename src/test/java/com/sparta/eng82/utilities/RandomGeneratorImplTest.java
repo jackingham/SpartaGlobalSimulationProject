a761ccb22cff78e3c64a54fb8b4b5680f17fa98f
Golam Choudhury <19grc97@gmail.com>
2021-04-15 15:18:14 +0100

    Created tests for the bootcamp class and added Display Names - Golam

--- src/test/java/com/sparta/eng82/utilities/RandomGeneratorImplTest.java
+++ src/test/java/com/sparta/eng82/utilities/RandomGeneratorImplTest.java
@@ -2,6 +2,7 @@ package com.sparta.eng82.utilities;
 
 
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -17,12 +18,14 @@ class RandomGeneratorImplTest {
     }
 
     @Test
+    @DisplayName("Testing if the generator returns the correct integer in a single value range")
     public void testCorrectReturn() {
         assertEquals(9, randomGenerator.randomInt(9, 10));
         // range 9 to 11 can return 9 or 10, but not 11
     }
 
     @Test
+    @DisplayName("Testing that that the returned value is in the specified range for the generator")
     public void testRangeIsCorrect() {
         int upper = 20;
         int lower = 10;
