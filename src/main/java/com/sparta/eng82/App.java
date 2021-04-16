60a58f7cba2209840df3942f77cdbf7e1ec0ba0a
abdulwm97 <abdulwm09@gmail.com>
2021-04-16 11:08:23 +0100

    removing main method from the UI. By Abdul, Teni and Jakub

--- src/main/java/com/sparta/eng82/App.java
+++ src/main/java/com/sparta/eng82/App.java
@@ -1,12 +1,28 @@
 package com.sparta.eng82;
-
-import com.sparta.eng82.controller.SimulationImpl;
-import com.sparta.eng82.utilities.InputExtension;
-import com.sparta.eng82.utilities.InputValidator;
+import com.sparta.eng82.view.UI.UI;
 
 public class App {
     public static void main(String[] args) {
-        SimulationImpl simulation = new SimulationImpl();
-        simulation.startSimulation(InputValidator.getUserInput(), InputExtension.outputEveryMonth());
+        new UI().start();
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//        SimulationImpl simulation = new SimulationImpl();
+//        simulation.startSimulation(InputValidator.getUserInput(), InputExtension.outputEveryMonth());
--- src/main/java/com/sparta/eng82/view/UI/UI.java
+++ src/main/java/com/sparta/eng82/view/UI/UI.java
@@ -1,7 +1,6 @@
 package com.sparta.eng82.view.UI;
 
 import com.sparta.eng82.controller.SimulationImpl;
-import com.sparta.eng82.utilities.InputValidator;
 import com.sparta.eng82.utilities.InputValidatorForUI;
 
 import javax.swing.*;
@@ -12,44 +11,42 @@ public class UI {
     String responseOne;
     String responseTwo;
     String responseThree;
-    String[] responses = new String[3];
+    String[] responses = new String[4];
+    JFrame frame1 = new JFrame("SpartaGlobal Academy Simulation");
+    JPanel mainPanel = new JPanel();
+    JPanel topPanel = new JPanel();
+    JPanel bottomPanel2 = new JPanel();
+    JPanel bottomPanel3 = new JPanel();
 
 
 
-    public static void main(String[] args){
-        new UI();
-    }
+    public void start(){
 
-    public UI(){
-        JFrame frame1 = new JFrame("SpartaGlobal Academy Simulation");
         frame1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame1.setSize(600, 250);
 
 
-        JPanel  mainPanel = new JPanel(); // main panel
+
         mainPanel.setLayout(new GridLayout(4, 1));
         mainPanel.setBackground(Color.white);
         mainPanel.setBorder(BorderFactory.createLineBorder(Color.black, 1));
 
-//        JPanel subPanel1 = new JPanel(); // sub-panel 1
-//        subPanel1.add(new JLabel("Panel One", SwingConstants.CENTER));
-
 
         Box titleText = Box.createHorizontalBox();
         JLabel title = new JLabel("<html><span style='color: teal;'>SpartaGlobal Academy Simulation</span></html>", SwingConstants.CENTER);
-        title.setFont (title.getFont().deriveFont(24.0f));
+        title.setFont(title.getFont().deriveFont(24.0f));
         titleText.add(title);
         titleText.setAlignmentX(frame1.getWidth() / 2);
 
-        JPanel topPanel = new JPanel();
+
         topPanel.add(titleText);
         topPanel.setBackground(Color.white);
 
 
-        JPanel bottomPanel2 = new JPanel();
+
         bottomPanel2.setBackground(Color.white);
         JLabel label1 = new JLabel("Are you entering months, years or a combination of both?\n");
-        label1.setFont (label1.getFont().deriveFont(12.0f));
+        label1.setFont(label1.getFont().deriveFont(12.0f));
         JRadioButton yearsRadio = new JRadioButton();
         JRadioButton monthsRadio = new JRadioButton();
         JRadioButton yAndMRadio = new JRadioButton();
@@ -59,8 +56,6 @@ public class UI {
         ButtonGroup yearsAndMonths = new ButtonGroup();
         JLabel label2 = new JLabel("Enter The time from in years and months to run the simulation:\n");
 
-        yearsAndMonths.add(yearsRadio);
-        yearsAndMonths.add(monthsRadio);
 
         bottomPanel2.add(label1);
         bottomPanel2.add(yearsLabel);
@@ -72,9 +67,8 @@ public class UI {
         bottomPanel2.add(label2);
 
 
-        JPanel bottomPanel3 = new JPanel();
         bottomPanel3.setBackground(Color.white);
-        JLabel monthlyOutputLabel = new JLabel("Would you like the output monthly ? \n",SwingConstants.CENTER);
+        JLabel monthlyOutputLabel = new JLabel("Would you like the output monthly ? \n", SwingConstants.CENTER);
         JRadioButton monthlyOutput = new JRadioButton();
         bottomPanel3.add(monthlyOutputLabel);
         bottomPanel3.add(monthlyOutput);
@@ -98,6 +92,8 @@ public class UI {
         mainPanel.add(bottomPanel4);
         mainPanel.add(bottomPanel3);
 
+
+
         reset.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 years.setText("");
@@ -105,47 +101,41 @@ public class UI {
             }
         });
 
+
         send.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                if( yearsRadio.isSelected()){
+                if (yearsRadio.isSelected()) {
                     responseOne = "y";
                     responses[0] = responseOne;
-                    if(years.getText().equals("")){
+                    if (years.getText().equals("")) {
                         responses[1] = "0";
-                    }
-                    else {
+                    } else {
                         responses[1] = years.getText();
                     }
                     responses[2] = "0";
-                }
-                else if( monthsRadio.isSelected()){
+                } else if (monthsRadio.isSelected()) {
                     responseTwo = "m";
                     responses[0] = responseTwo;
                     responses[1] = "0";
-                    if(months.getText().equals("")){
+                    if (months.getText().equals("")) {
                         responses[2] = "0";
-                    }
-                    else {
+                    } else {
                         responses[2] = months.getText();
                     }
-                }
-                else if( yAndMRadio.isSelected()){
+                } else if (yAndMRadio.isSelected()) {
                     responseThree = "ym";
                     responses[0] = responseThree;
-                    if(years.getText().equals("")){
+                    if (years.getText().equals("")) {
                         responses[1] = "0";
-                    }
-                    else {
+                    } else {
                         responses[1] = years.getText();
                     }
-                    if(months.getText().equals("")){
+                    if (months.getText().equals("")) {
                         responses[2] = "0";
-                    }
-                    else {
+                    } else {
                         responses[2] = months.getText();
                     }
-                }
-                else{
+                } else {
                     responses[0] = "m";
                     responses[1] = "0";
                     responses[2] = "0";
@@ -153,7 +143,7 @@ public class UI {
 
 
                 SimulationImpl simulation = new SimulationImpl();
-                simulation.startSimulation(InputValidatorForUI.getUserInput(responses[0], Integer.parseInt(responses[1]),Integer.parseInt(responses[2])), monthlyOutput.isSelected());
+                simulation.startSimulation(InputValidatorForUI.getUserInput(responses[0], Integer.parseInt(responses[1]), Integer.parseInt(responses[2])), monthlyOutput.isSelected());
 
             }
         });
